5be86fcc4a382f3e69b7678941bcdbe982caec79
EljiahColwill <elijah.c035@gmail.com>
2018-03-29 12:19:09 -0600

    Activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -1,13 +1,29 @@
-/**
- * This is a class that tests the Card class.
- */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("King", "Hearts", 13);
+		Card card2 = new Card("Queen", "Hearts", 12);
+		Card card3 = new Card("4", "Spades", 4);
+
+		System.out.println(card1.suit());
+		System.out.println(card2.suit());
+		System.out.println(card3.suit());
+
+		System.out.println(card1.rank());
+		System.out.println(card2.rank());
+		System.out.println(card3.rank());
+
+		System.out.println(card1.pointValue());
+		System.out.println(card2.pointValue());
+		System.out.println(card3.pointValue());
+
+		System.out.println(card1.matches(card2));
+		System.out.println(card2.matches(card1));
+		System.out.println(card3.matches(card3));
+
+		System.out.println(card1.toString());
+		System.out.println(card2.toString());
+		System.out.println(card3.toString());
+
 	}
 }
